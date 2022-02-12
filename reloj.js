0bed88be1161f73dd7583adf232e54dba0335bea
lordkendri <76025241+lordkendri@users.noreply.github.com>
2022-02-11 23:35:34 -0400

    commit1

---
+++ reloj.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const addZero = (n)=>{
+    if (n.toString().length < 2) return "0".concat(n);
+    return n;
+}
+
+const actualizarHora = ()=>{
+    const time = new Date();
+    let hora = addZero(time.getHours());
+    let min = addZero(time.getMinutes());
+    let seg = addZero(time.getSeconds());
+    document.querySelector(".hora").textContent = hora;
+    document.querySelector(".min").textContent = min;
+    document.querySelector(".seg").textContent = seg;
+}
+
+actualizarHora();
+setInterval(actualizarHora,1000);   
